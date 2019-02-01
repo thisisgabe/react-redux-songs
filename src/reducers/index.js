a4436502d8c5ca82415e020be7c45e3b9e13d8c9
Gabe Velasquez <gvelasquez@trackforce.com>
2019-02-01 15:16:33 -0800

    used provider and passed store to it

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from "redux";
+
 const songsReducer = () => {
   return [
     { title: "Shuggie", artist: "Foxygen", duration: "3:22" },
@@ -14,3 +16,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
 
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});

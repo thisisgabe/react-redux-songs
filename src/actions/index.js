147e9012ee8a1f746d42de926fbfb01da893147d
Gabe Velasquez <gvelasquez@trackforce.com>
2019-02-01 15:10:35 -0800

    created actions and reducers

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+//Actions creator
+
+export const selectSong = song => {
+  //return an action
+  return {
+    type: "SONG_SELECTED",
+    payload: song
+  };
+};
---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+const songsReducer = () => {
+  return [
+    { title: "Shuggie", artist: "Foxygen", duration: "3:22" },
+    { title: "Raspberry", artist: "Grouplove", duration: "3:16" },
+    { title: "Going Native", artist: "Summer Salt", duration: "5:11" },
+    { title: "Leave It In My Dreams", artist: "The Voidz", duration: "3:59" }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
